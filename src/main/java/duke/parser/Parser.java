2be645f206df723648475be3c2c0abac2a023bf6
Robinho98 <ho.robin@yahoo.com.sg>
2020-09-17 11:34:46 +0800

    Add try catch block to parser in case user inputs the wrong formatting

--- src/main/java/duke/parser/Parser.java
+++ src/main/java/duke/parser/Parser.java
@@ -24,50 +24,55 @@ public class Parser {
      * @return the command based on the user input.
      */
     public static Command parse(String command) throws DukeException {
-        String[] parts = command.split(" ", 2);
-        switch (parts[0].toLowerCase()) {
-        case "bye":
-            return new ExitCommand();
-        case "list":
-            return new ListCommand();
-        case "done":
-            String doneTaskIndex = parts[1].trim();
-            return new DoneCommand(Integer.parseInt(doneTaskIndex));
-        case "delete":
-            String deleteTaskIndex = parts[1].trim();
-            return new DeleteCommand(Integer.parseInt(deleteTaskIndex));
-        case "tag":
-            String[] tag = parts[1].split(" ");
-            String tagTaskIndex = tag[0].trim();
-            String tagDesription = tag[1].trim();
-            return new TagCommand(Integer.parseInt(tag[0]), tag[1]);
-        case "find":
-            String keyword = parts[1].trim();
-            return new FindCommand(keyword);
-        case "todo":
-            String toDoDescription = parts[1].trim();
-            return new ToDoCommand(toDoDescription);
-        case "deadline":
-            try {
-                String[] deadline = parts[1].split("/by");
-                String deadlineDescription = deadline[0].trim();
-                String dateBy = deadline[1].trim();
-                return new DeadlineCommand(deadlineDescription, dateBy);
-            } catch (Exception e) {
-                throw new DukeException("Deadline formatting is wrong! Please include /by.");
-            }
-        case "event":
-            try {
-                String[] event = parts[1].split("/at");
-                String eventDescription = event[0].trim();
-                String dateAt = event[1].trim();
-                return new EventCommand(eventDescription, dateAt);
-            } catch (Exception e) {
-                throw new DukeException("Event formatting is wrong! Please include /at.");
-            }
+        try {
+            String[] parts = command.split(" ", 2);
+            switch (parts[0].toLowerCase()) {
+            case "bye":
+                return new ExitCommand();
+            case "list":
+                return new ListCommand();
+            case "done":
+                String doneTaskIndex = parts[1].trim();
+                return new DoneCommand(Integer.parseInt(doneTaskIndex));
 
-        default:
-            throw new DukeException("Please try again!");
+            case "delete":
+                String deleteTaskIndex = parts[1].trim();
+                return new DeleteCommand(Integer.parseInt(deleteTaskIndex));
+            case "tag":
+                String[] tag = parts[1].split(" ");
+                String tagTaskIndex = tag[0].trim();
+                String tagDesription = tag[1].trim();
+                return new TagCommand(Integer.parseInt(tag[0]), tag[1]);
+            case "find":
+                String keyword = parts[1].trim();
+                return new FindCommand(keyword);
+            case "todo":
+                String toDoDescription = parts[1].trim();
+                return new ToDoCommand(toDoDescription);
+            case "deadline":
+                try {
+                    String[] deadline = parts[1].split("/by");
+                    String deadlineDescription = deadline[0].trim();
+                    String dateBy = deadline[1].trim();
+                    return new DeadlineCommand(deadlineDescription, dateBy);
+                } catch (Exception e) {
+                    throw new DukeException("Deadline formatting is wrong! Please include /by.");
+                }
+            case "event":
+                try {
+                    String[] event = parts[1].split("/at");
+                    String eventDescription = event[0].trim();
+                    String dateAt = event[1].trim();
+                    return new EventCommand(eventDescription, dateAt);
+                } catch (Exception e) {
+                    throw new DukeException("Event formatting is wrong! Please include /at.");
+                }
+
+            default:
+                throw new DukeException("Please try again!");
+            }
+        } catch (Exception e) {
+            throw new DukeException("Formatting is wrong! Please try again.");
         }
     }
 }
